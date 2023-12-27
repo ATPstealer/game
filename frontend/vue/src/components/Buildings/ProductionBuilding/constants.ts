a9f11f69f0750b976366d020983930f8e0bcb272
atpstealer <forpubmail@gmail.com>
2023-12-27 09:23:10 +0200

    Work duration ns -> seconds

--- frontend/vue/src/components/Buildings/ProductionBuilding/constants.ts
+++ frontend/vue/src/components/Buildings/ProductionBuilding/constants.ts
@@ -1,26 +1,26 @@
 export const timeValues = [
   {
-    value: 3600 * 10000000000,
+    value: 3600 * 1000000000,
     label: '1 hour'
   },
   {
-    value: 7200 * 10000000000,
+    value: 7200 * 1000000000,
     label: '2 hours'
   },
   {
-    value: 18000 * 10000000000,
+    value: 18000 * 1000000000,
     label: '5 hours'
   },
   {
-    value: 86400 * 10000000000,
+    value: 86400 * 1000000000,
     label: '24 hours'
   },
   {
-    value: 604800 * 10000000000,
+    value: 604800 * 1000000000,
     label: '7 days'
   },
   {
-    value: 2592000 * 10000000000,
+    value: 2592000 * 1000000000,
     label: '30 days'
   }
 ]
