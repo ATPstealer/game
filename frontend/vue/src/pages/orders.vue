1448afe88e62acc23b81fcea4288bc0dada36f56
mav <atpstealer@gmail.com>
2024-01-07 19:14:40 +0000

    Orders

--- frontend/vue/src/pages/orders.vue
+++ frontend/vue/src/pages/orders.vue
@@ -1,67 +1,17 @@
 <template>
   <Layout>
-    <div v-if="!isFetching">
-      <h2 class="font-bold text-2xl">
-        Orders
-      </h2>
-      <div class="grid grid-cols-5 gap-2 font-bold">
-        <h3 class="col-span-1">
-          Cell
-        </h3>
-        <h3 class="col-span-1">
-          Resource
-        </h3>
-        <h3 class="col-span-1">
-          Amount
-        </h3>
-        <h3 class="col-span-1">
-          Price
-        </h3>
-        <h3 class="col-span-1">
-          Type (Close)
-        </h3>
-      </div>
-      <div
-        v-for="order in orders"
-        :key="order.id"
-        class="grid grid-cols-5 "
-      >
-        <p class="col-span-1">
-          {{ order.x }}:{{ order.y }}
-        </p>
-        <p class="col-span-1">
-          {{ order.resourceName }}
-        </p>
-        <p class="col-span-1">
-          {{ order.amount }}
-        </p>
-        <p class="col-span-1">
-          {{ order.priceForUnit }}$
-        </p>
-        <Button
-          text
-          :label="order.sell ? 'sell' : 'buy'"
-          @click="closeOrder(order.id)"
-          class="text-red-600 font-bold col-span-1"
-        />
-      </div>
-    </div>
-    <Loading v-else />
+    <Orders />
+    <template #help>
+      {{ t(`orders.help`) }}
+    </template>
   </Layout>
 </template>
 
 <script setup lang="ts">
-import Button from 'primevue/button'
+import { useI18n } from 'vue-i18n'
 import Layout from '@/components/Common/Layout.vue'
-import Loading from '@/components/Common/Loading.vue'
-import { useGetData } from '@/composables/useGetData'
-import { useOrders } from '@/composables/useOrders'
-import type { Order } from '@/types'
+import Orders from '@/components/Orders/Orders.vue'
 
-const { data: orders, isFetching } = useGetData<Order[]>('/market/order/my')
-const { closeOrder } = useOrders()
+const { t } = useI18n()
+
 </script>
-
-<style scoped>
-
-</style>
