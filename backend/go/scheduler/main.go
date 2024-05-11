49b65263f971d16997ba35983cad451978cf748a
atpstealer <forpubmail@gmail.com>
2024-05-11 10:38:27 +0200

    Scheduler to timer as application

--- backend/go/scheduler/main.go
+++ backend/go/scheduler/main.go
@@ -5,12 +5,22 @@ import (
 	"backend/packages/db"
 	"backend/scheduler/gameLive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"log"
+	"time"
 )
 
 func main() {
 	cfg.LoadConfig()            // global cfg.Config
 	db.MongoConnect(cfg.Config) // global db.M
-	alive(db.M)
+	for {
+		start := time.Now()
+		log.Printf(start.String())
+		alive(db.M)
+
+		select {
+		case <-time.After(time.Until(start.Add(1 * time.Minute))):
+		}
+	}
 }
 
 func alive(m *mongo.Database) {
