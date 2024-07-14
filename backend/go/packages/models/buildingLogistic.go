a1384d35b804dbf6931be0eba3c70f8a14b0edb2
atpstealer <forpubmail@gmail.com>
2024-07-14 19:22:34 +0200

    Add logistic building capacity feature

--- backend/go/packages/models/buildingLogistic.go
+++ backend/go/packages/models/buildingLogistic.go
@@ -1,10 +1,17 @@
 package models
 
+import (
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
 type Logistics struct {
 	CapacityMax float64 `json:"capacityMax" bson:"capacityMax"`
 	Capacity    float64 `json:"capacity" bson:"capacity"`
 	Speed       float64 `json:"speed" bson:"speed"`
 	Price       float64 `json:"price" bson:"price"`
+	Revenue     float64 `json:"revenue" bson:"revenue"`
 }
 
-//func LogisticsRecount(m *mongo.Database)
+func LogisticsRecount(m *mongo.Database) {
+
+}
