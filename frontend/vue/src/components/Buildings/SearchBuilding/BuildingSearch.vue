90d79fca29117d0ac7d5c583ac4131e259481a37
Nomel <nomelnomel@gmail.com>
2024-10-27 22:45:53 +0300

    поиск зданий, обновление апи и новый выбор координат

--- frontend/vue/src/components/Buildings/SearchBuilding/BuildingSearch.vue
+++ frontend/vue/src/components/Buildings/SearchBuilding/BuildingSearch.vue
@@ -1,12 +1,8 @@
 <template>
   <DataTable
-    v-if="!isFetching"
-    :value="buildings as any[]"
-    striped-rows
-    paginator
-    :rows="10"
-    :rows-per-page-options="[10, 25]"
+    v-if="!isPending"
     class="p-4 w-full"
+    paginator
     :pt="{paginator:
       {
         pagebutton: {class: 'paginator-button'},
@@ -16,60 +12,80 @@
         nextpagebutton: {class: 'paginator-button'},
       },
     }"
+    :rows="10"
+    :rows-per-page-options="[10, 25]"
+    striped-rows
+    :value="buildings as any[]"
   >
     <Column
+      class="flex-2"
       field="title"
       :header="t('buildings.one')"
       :sortable="true"
-      class="flex-2"
     >
       <template #body="{data}">
         <span>{{ t(`buildings.types.${data?.buildingType.title.toLocaleLowerCase()}`) }}</span>
       </template>
     </Column>
     <Column
+      class="flex-1"
       field="nickName"
       :header="t('common.owner')"
       :sortable="true"
-      class="flex-1"
     />
     <Column
+      class="flex-1"
       field="square"
       :header="t('common.square')"
       :sortable="true"
-      class="flex-1"
     />
     <Column
+      class="flex-1"
       field="x"
       header="X"
-      class="flex-1"
     />
     <Column
+      class="flex-1"
       field="y"
       header="Y"
-      class="flex-1"
     />
   </DataTable>
 </template>
 
 <script setup lang="ts">
+import { useMutation } from '@tanstack/vue-query'
 import Column from 'primevue/column'
 import DataTable from 'primevue/datatable'
-import { toRefs } from 'vue'
+import { onMounted, ref, toRefs, watch } from 'vue'
 import { useI18n } from 'vue-i18n'
-import { useBuildings } from '@/composables/useBuildings'
-import type { SearchBuildingParams } from '@/types/Buildings/index.interface'
+import type { BuildingWithData, FindBuildingParams } from '@/api'
+import { postBuildingGetMutation } from '@/api/@tanstack/vue-query.gen'
 
 interface Props {
-  searchParams: SearchBuildingParams;
+  searchParams: FindBuildingParams;
 }
 
 const props = defineProps<Props>()
 const { searchParams } = toRefs(props)
 
+const buildings = ref<BuildingWithData[]>([])
+
 const { t } = useI18n()
-const { getBuildings } = useBuildings()
-const { data: buildings, isFetching } = getBuildings(searchParams)
+
+const { isPending, mutate } = useMutation({
+  ...postBuildingGetMutation(),
+  onSuccess: (data: any) => {
+    buildings.value = data.data
+  }
+})
+
+watch(searchParams.value, () => {
+  mutate({ body: { ...searchParams.value } })
+})
+
+onMounted(() => {
+  mutate({ body: { ...searchParams.value } })
+})
 
 </script>
 
